1962edcc9547035b06b1329cacf9c61fac32676f
Bobbypip <51970655+Bobbypip@users.noreply.github.com>
2020-06-13 22:45:36 -0500

    Database_Seeding

--- ApplicationDbContext.cs
+++ ApplicationDbContext.cs
@@ -5,12 +5,35 @@ namespace GURU_CoMM
 {
     public class ApplicationDbContext : DbContext
     {
-            public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
-                : base(options)
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            var pet1 = new Pet()
             {
+                Id = 1,
+                Name = "Luffy"
+            };
+            var pet2 = new Pet()
+            {
+                Id = 2,
+                Name = "Ronny"
+            };
+            var pet3 = new Pet()
+            {
+                Id = 3,
+                Name = "Senpai"
+            };
 
-            }
+            modelBuilder.Entity<Pet>().HasData(pet1);
+            modelBuilder.Entity<Pet>().HasData(pet2);
+            modelBuilder.Entity<Pet>().HasData(pet3);
+        }
 
-            public DbSet<Pet> Pets { get; set; }
+        public DbSet<Pet> Pets { get; set; }
     }
 }
--- Migrations/20200614034027_DB_Sedding.Designer.cs
+++ Migrations/20200614034027_DB_Sedding.Designer.cs
@@ -3,14 +3,16 @@ using GURU_CoMM;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace GURU_CoMM.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20200614034027_DB_Sedding")]
+    partial class DB_Sedding
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
